3c44789ecfd5b0a0f4fb42ef8e9d9e1e9c73a99f
kholisfathon1310-tec <kholisfathon1310@gmail.com>
2026-05-04 05:01:52 +0700

    setting database

--- Database.php
+++ Database.php
@@ -1,16 +1,16 @@
 <?php
 class Database {
-    private $host = "localhost"; // Ganti dengan host database kamu
-    private $db_name = "penggajian"; // Nama database
-    private $username = "root"; // Username database
-    private $password = ""; // Password database
+    private $host = "maleo"; // Ganti dengan host database kamu
+    private $db_name = "adkcloud_Penggajian1"; // Nama database
+    private $username = "adkcloud_admin"; // Username database
+    private $password = "9G8URw.EiQ;SankA"; // Password database
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . "adkcloud_penggajian" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
